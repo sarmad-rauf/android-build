092706c8f3cf7adb864909efa66e1a759922b5ec
Usama Alvi <usama.nasir@evampsaanga.com>
2020-07-17 23:12:45 +0500

    Consumer Registration OTP Lenght and Validation Regex Added

--- app/src/main/java/com/es/marocapp/usecase/consumerregistration/fragments/ConsumerRegistrationVerifyOtpFragment.kt
+++ app/src/main/java/com/es/marocapp/usecase/consumerregistration/fragments/ConsumerRegistrationVerifyOtpFragment.kt
@@ -1,7 +1,11 @@
 package com.es.marocapp.usecase.consumerregistration.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.es.marocapp.R
@@ -13,12 +17,17 @@ import com.es.marocapp.usecase.MainActivity
 import com.es.marocapp.usecase.consumerregistration.ConsumerRegistrationActivity
 import com.es.marocapp.usecase.consumerregistration.ConsumerRegistrationClickListner
 import com.es.marocapp.usecase.consumerregistration.ConsumerRegistrationViewModel
+import com.es.marocapp.utils.Constants
 import com.es.marocapp.utils.DialogUtils
+import java.util.regex.Pattern
 
 class ConsumerRegistrationVerifyOtpFragment : BaseFragment<FragmentConsumerRegistrationVerifyOtpBinding>(),
-    ConsumerRegistrationClickListner {
+    ConsumerRegistrationClickListner, TextWatcher {
 
     lateinit var mActivityViewModel: ConsumerRegistrationViewModel
+
+    var isOTPRegexMatches = false
+
 
     override fun setLayout(): Int {
         return R.layout.fragment_consumer_registration_verify_otp
@@ -39,6 +48,11 @@ class ConsumerRegistrationVerifyOtpFragment : BaseFragment<FragmentConsumerRegis
         mDataBinding.txtResend.setOnClickListener {
             mActivityViewModel.requestForGetOTPForRegistrationApi(activity,mActivityViewModel.firstName,mActivityViewModel.lastName,mActivityViewModel.identificationNumber)
         }
+
+        mDataBinding.inputVerifyOtp.filters =
+            arrayOf<InputFilter>(InputFilter.LengthFilter(Constants.APP_OTP_LENGTH))
+
+        mDataBinding.inputVerifyOtp.addTextChangedListener(this)
 
         setStrings()
         subscribeObserver()
@@ -91,12 +105,21 @@ class ConsumerRegistrationVerifyOtpFragment : BaseFragment<FragmentConsumerRegis
             mDataBinding.inputLayoutVerifyOtp.error = ""
             mDataBinding.inputLayoutVerifyOtp.isErrorEnabled = false
 
+            if(isOTPRegexMatches){
+                mDataBinding.inputLayoutVerifyOtp.error = ""
+                mDataBinding.inputLayoutVerifyOtp.isErrorEnabled = false
+
                 mActivityViewModel.requestForRegisterUserApi(
                     activity,
                     "",
                     mDataBinding.inputVerifyOtp.text.toString().trim()
                 )
 
+
+            }else{
+                mDataBinding.inputLayoutVerifyOtp.error = LanguageData.getStringValue("PleaseEnterValidOTP")
+                mDataBinding.inputLayoutVerifyOtp.isErrorEnabled = true
+            }
         }
     }
 
@@ -106,4 +129,16 @@ class ConsumerRegistrationVerifyOtpFragment : BaseFragment<FragmentConsumerRegis
     override fun onGenderSelectionClick(view: View) {
     }
 
+    override fun afterTextChanged(p0: Editable?) {
+        var otp = mDataBinding.inputVerifyOtp.text.toString().trim()
+        var otpLenght = otp.length
+        isOTPRegexMatches =
+            (otpLenght > 0 && Pattern.matches(Constants.APP_OTP_REGEX, otp))
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
 }
