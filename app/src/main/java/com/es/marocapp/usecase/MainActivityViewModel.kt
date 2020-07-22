3a6bbdabd7f1596028bf148e46a18a99f10b37a4
Zia <zia.malik@evampsaanga.com>
2020-07-22 12:11:06 +0500

    jenkins fixes testing

--- app/src/main/java/com/es/marocapp/usecase/MainActivityViewModel.kt
+++ app/src/main/java/com/es/marocapp/usecase/MainActivityViewModel.kt
@@ -41,7 +41,7 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
                     { result ->
                         isLoading.set(false)
 
-                        if (result?.responseCode != null && result?.responseCode!!.equals(
+                      /*  if (result?.responseCode != null && result?.responseCode!!.equals(
                                 ApiConstant.API_SUCCESS, true
                             )
                         ) {
@@ -49,7 +49,14 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
 
                         } else {
                             getLogOutUserResponseListner.postValue(result)
+                        }*/
+
+                        if (result?.responseCode != null )
+                         {
+                            getLogOutUserResponseListner.postValue(result)
+
                         }
+
 
 
                     },
